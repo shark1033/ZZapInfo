30c6471e08f1915f93ea40523c832b11f8be9f93
shark1033 <shark10331@gmail.com>
2018-09-13 09:40:34 +0300

    1

--- src/main/java/Excel.java
+++ src/main/java/Excel.java
@@ -11,16 +11,22 @@ public class Excel {
     private int priceColumn;
     private int plusColumn;
 
+    private int ourPriceColumn;
+    private int firstPriceColumn;
+    private int ourPositionColumn;
+    private int absPriceColumn;
+    private int firstCompanyColumn;
+
     private HSSFWorkbook myExcelBook;
     private HSSFSheet myExcelSheet;
     private HSSFCellStyle[] style = new HSSFCellStyle[3];
     private FileInputStream file;
-    private HSSFCellStyle style1, style2, style3;
+    private HSSFCellStyle style1, style2, style3, style4;
 
     //загрузка документа и создания экземпляров
     public void init() {
         try {
-            file = new FileInputStream("C:\\Users\\User\\Desktop\\Прайсы\\new.xls");
+            file = new FileInputStream("C:\\Users\\User\\Desktop\\Прайсы\\new2.xls");
             myExcelBook = new HSSFWorkbook(file);
             myExcelSheet = myExcelBook.getSheet("Лист1");
         } catch (FileNotFoundException e) {
@@ -33,7 +39,7 @@ public class Excel {
     //перезаписываем и закрваем файл
     public void saveAndClose() {
         try {
-            myExcelBook.write(new FileOutputStream(new File("C:\\Users\\User\\Desktop\\Прайсы\\new.xls")));
+            myExcelBook.write(new FileOutputStream(new File("C:\\Users\\User\\Desktop\\Прайсы\\new2.xls")));
             file.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,35 +52,51 @@ public class Excel {
         createStyles();
         for (int i = rowStart; i < rows; i++) {
             HSSFCell cell = myExcelSheet.getRow(i).createCell(column);
-            cell.setCellStyle(style1);
+            //cell.setCellStyle(style1);
         }
     }
     //устанавливаем стили ячейкам
-    public void setStyles(int rowStart, int column) {
-        int rows = myExcelSheet.getLastRowNum();
-        createStyles();
-        for (int i = rowStart; i < rows; i++) {
-            HSSFCell cell = myExcelSheet.getRow(i).getCell(column);
-            cell.setCellStyle(style1);
+    public void setStyles(int rowStart,int rowEnd, int ...arg) {
+        for (int i = rowStart; i < rowEnd; i++) {
+//            for (int x:arg) {
+//                HSSFCell cell = myExcelSheet.getRow(i).getCell(arg[x]);
+//                cell.setCellStyle(styleAlign);
+//            }
+            for (int y=arg[0];y<arg.length;y++){
+                HSSFCell cell = myExcelSheet.getRow(i).getCell(y);
+                cell.getCellStyle().setAlignment(HSSFCellStyle.ALIGN_CENTER);
+            }
         }
     }
+
+
+
+
+
     // создаём стили для ячеек
     public void createStyles() {
         //Дороже конкурента
         style1 = myExcelBook.createCellStyle();
-        style1.setFillBackgroundColor(HSSFColor.RED.index);
-        style1.setAlignment(HSSFCellStyle.ALIGN_FILL);
+        style1.setAlignment(HSSFCellStyle.ALIGN_CENTER);
         style1.setFillForegroundColor(HSSFColor.RED.index);
-        style1.setFillPattern( HSSFCellStyle.ALIGN_FILL);
+        style1.setFillPattern( HSSFCellStyle.SOLID_FOREGROUND);
         //Примерно также как у конкурента или равно
         style2 = myExcelBook.createCellStyle();
-        style2.setFillBackgroundColor(HSSFColor.YELLOW.index);
-        style2.setAlignment(HSSFCellStyle.ALIGN_FILL);
+        style2.setFillForegroundColor(HSSFColor.YELLOW.index);
+       style2.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        style2.setFillPattern( HSSFCellStyle.SOLID_FOREGROUND);
         //меньше чем у конкурента
         style3 = myExcelBook.createCellStyle();
-        style3.setFillBackgroundColor(HSSFColor.GREEN.index);
-        style3.setAlignment(HSSFCellStyle.ALIGN_FILL);
+        style3.setFillForegroundColor(HSSFColor.GREEN.index);
+        style3.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        style3.setFillPattern( HSSFCellStyle.SOLID_FOREGROUND);
+        //нет на ззапе либо что-то не так
+        style4 = myExcelBook.createCellStyle();
+        style4.setFillForegroundColor(HSSFColor.BLACK.index);
+        style4.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        style4.setFillPattern( HSSFCellStyle.SOLID_FOREGROUND);
     }
+
     //читаем информацию с листа
     public void read(int rowStart, int rowEnd, int column, int column2, int column3) {
         System.out.println(myExcelSheet.getLastRowNum());
@@ -83,13 +105,13 @@ public class Excel {
             HSSFRow row = myExcelSheet.getRow(i);
 
                 String name1 = row.getCell(column).getStringCellValue();
-                System.out.print("MAKER2: "+name1);
+                System.out.print("      maker : "+name1);
 
                 String name2 = row.getCell(column2).getStringCellValue();
-                System.out.print("    oem2 : " + name2);
+                System.out.print("      oem2 : " + name2);
 
                 double number = row.getCell(column3).getNumericCellValue();
-                System.out.print("    price1: " + number);
+                System.out.print("      price : " + number);
 
             System.out.println("\n");
         }
@@ -109,26 +131,120 @@ public class Excel {
         HSSFRow row = myExcelSheet.getRow(rowIndex);
 
             String name = row.getCell(makerColumn).getStringCellValue();
-            System.out.print("MAKER: "+name);
+            System.out.print("  MAKER: "+name);
             return name;
     }
 
     public String readOEM(int rowIndex){
         HSSFRow row = myExcelSheet.getRow(rowIndex);
             String name = row.getCell(oemColumn).getStringCellValue();
+            if(name.contains("/")){
+                int count=name.indexOf("/");
+                name=name.substring(0,count);
+
+            }
             System.out.print("OEM: "+name);
             return name;
     }
     public double readPrice(int rowIndex){
         HSSFRow row = myExcelSheet.getRow(rowIndex);
             double price=row.getCell(priceColumn).getNumericCellValue();
-            System.out.print("Price: "+price);
+            System.out.print("  Price: "+price);
             return price;
+    }
+
+    public void writeToExcel(int rowIndex, double ourPrice,double[] minPrices,String firstCompany,boolean isonzzap){
+        System.out.println("Наичнаю запись...");
+        System.out.println("isonzzap "+isonzzap);
+
+        HSSFRow row = myExcelSheet.getRow(rowIndex);
+        row.getCell(ourPriceColumn).setCellValue(ourPrice);
+        row.getCell(firstPriceColumn).setCellValue(minPrices[0]);
+        System.out.println("ourprice: "+ ourPrice);
+        System.out.println("minrprice: "+ minPrices[0]);
+
+        double abs;
+
+        if(ourPrice>minPrices[0]){
+            abs=ourPrice-minPrices[0];
+            System.out.println("abs: " +abs);
+            row.getCell(absPriceColumn).setCellValue(abs);
+            row.getCell(absPriceColumn).setCellStyle(chooseStyle(abs,isonzzap));
+            System.out.println("условие 1");
+        }
+        else if(ourPrice==minPrices[0]){
+            abs=minPrices[0]-minPrices[1]; //no!
+            System.out.println("abs: " +abs);
+            row.getCell(absPriceColumn).setCellValue(abs);
+            row.getCell(absPriceColumn).setCellStyle(chooseStyle(abs,isonzzap));
+            System.out.println("условие 2");
+        }
+        else {
+            //abs=minPrices[0]-ourPrice;
+            //row.getCell(absPriceColumn).setCellValue(abs);
+            row.getCell(absPriceColumn).setCellStyle(style4);
+            System.out.println("условие 3");
+        }
+
+        row.getCell(firstCompanyColumn).setCellValue(firstCompany);
+        if(ourPrice==minPrices[0]){
+            row.getCell(ourPositionColumn).setCellValue(1);
+        }
+        else{
+            row.getCell(ourPositionColumn).setCellValue(0);
+        }
+
+
+
+    }
+
+    public HSSFCellStyle chooseStyle(double abs,boolean isonzzap) {
+        if(isonzzap) {
+
+            if (abs > 1000) { //red
+                System.out.println("стиль1");
+                return style1;
+            } else if (abs > 0) {
+                System.out.println("стиль2");
+                return style2;
+            } else if (abs < 0) {
+                System.out.println("стиль3");
+                return style3;
+            }
+        }
+        else {
+            System.out.println("стиль4");
+            return style4;
+        }
+        System.out.println("no styles");
+        return null;
     }
 
 
 
     //геттеры и сеттеры
+
+
+    public void setFirstCompanyColumn(int firstCompanyColumn) {
+        this.firstCompanyColumn = firstCompanyColumn;
+    }
+
+    public void setOurPriceColumn(int ourPriceColumn) {
+        this.ourPriceColumn = ourPriceColumn;
+    }
+
+    public void setFirstPriceColumn(int firstPriceColumn) {
+        this.firstPriceColumn = firstPriceColumn;
+    }
+
+    public void setOurPositionColumn(int ourPositionColumn) {
+        this.ourPositionColumn = ourPositionColumn;
+    }
+
+    public void setAbsPriceColumn(int absPriceColumn) {
+        this.absPriceColumn = absPriceColumn;
+    }
+
     public void setOemColumn(int oemColumn) {
         this.oemColumn = oemColumn;
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,40 +9,36 @@ import java.util.HashMap;
 
 public class Main {
 
-    private static double[] price=new double[20];
-    private static GetJson getJson2;
-
+    //private static double[] price = new double[20];
+    //private static double priceExcel;
 
     public static void main(String[] args) {
         startProgramm();
 
     }
 
-    public static void getJson(String part_number, String class_man){
+    public static void getJson(String part_number, String class_man, final int i, final Excel excel, final double priceEx) {
         //ConnectToZZap.getZzapApi().getInfo("164000H120", "TOYOTA").enqueue(new Callback<ResponseBody>() {
         ConnectToZZap.getZzapApi().getInfo(part_number, class_man).enqueue(new Callback<ResponseBody>() {
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
 
                 System.out.println("Start printing");
                 try {
-                    String s=response.body().string();
-                    s=cutResponse(s);
+                    String s = response.body().string();
+                    s = cutResponse(s);
                     System.out.println(s);
                     Gson gson = new Gson();
 
-                    //Type collectionType = new TypeToken<GetJson>(){}.getType();
-                    getJson2 = gson.fromJson(s, GetJson.class);
-
-//                    for (Table itrTable : getJson2.getTable()) {
-//                        System.out.println(itrTable.getPrice());
-//                        //price[]=Double.valueOf(itrTable.getPrice());
-//                    }
-
+                    GetJson getJson2 = gson.fromJson(s, GetJson.class);
+                    double[] prices = getPrices(getJson2); //получили две цены
+                    excel.writeToExcel(i, priceEx, prices, getCompany(getJson2),isOnZzap(getJson2));
+                    System.out.println("finished with row № "+(i+1));
 
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
+
             public void onFailure(Call<ResponseBody> call, Throwable throwable) {
                 System.out.println("Errors!");
                 throwable.printStackTrace();
@@ -50,47 +46,49 @@ public class Main {
         });
     }
 
-    private static void startProgramm(){
+    private static void startProgramm() {
 
         String part_number;
         String class_man;
 
         Excel excel = new Excel();
         excel.init();
+        excel.createStyles();
         excel.setMakerColumn(1);
         excel.setOemColumn(2);
         excel.setPriceColumn(10);
         excel.setPlusColumn(6);
-        int rowStart=7;
-        int rowEnd=2216;
+        excel.setOurPriceColumn(12);
+        excel.setFirstPriceColumn(13);
+        excel.setFirstCompanyColumn(14);
+        excel.setAbsPriceColumn(15);
+        excel.setOurPositionColumn(16);
+        int rowStart = 30;
+        int rowEnd = 55;
+        excel.createCells(rowStart, 12);
+        excel.createCells(rowStart, 13);
+        excel.createCells(rowStart, 14);
+        excel.createCells(rowStart, 15);
+        excel.createCells(rowStart, 16);
 
-        String[] arrayExcel=new String[2];
-        double priceExcel;
+        String[] arrayExcel = new String[2];
 
 
-        for (int i=rowStart;i<rowEnd;i++) {
-            if(!excel.readPlus(i)) {
+        for (int i = rowStart; i < rowEnd; i++) {
+            if (!excel.readPlus(i)) {
                 continue;
             }
 
-            arrayExcel[0]=excel.readOEM(i);
-            arrayExcel[1]=excel.readMaker(i);
-            priceExcel=excel.readPrice(i);
+            arrayExcel[0] = excel.readOEM(i);
+            arrayExcel[1] = excel.readMaker(i);
+            double priceExcel = excel.readPrice(i);
 
-            getJson(arrayExcel[0],arrayExcel[1]);
+            getJson(arrayExcel[0], arrayExcel[1], i, excel, priceExcel);
+            wait3();
 
-            int y=0;
-            for (Table itrTable : getJson2.getTable()){
-                price[y]=Double.valueOf(itrTable.getPrice());
-                System.out.println(price[y]);
-                y++;
-            }
-
-            for(int t=0;t<4000;t++){
-                System.out.println("@");
-            }
-
-
+//            for(int t=0;t<4000;t++){
+//                System.out.println("@");
+//            }
 
 
         }
@@ -98,16 +96,104 @@ public class Main {
 //        excel.createStyles();
 //        excel.createCells(7,11);
 //        excel.read( 7, 2216, excel.getMakerColumn(), excel.getOemColumn(), excel.getPriceColumn());
+        //excel.setStyles(rowStart, rowEnd, 12, 13, 14);
         excel.saveAndClose();
+    }
+
+    public static double[] getPrices(GetJson getJson2) {
+        int y = 0;
+        double[] price = new double[10];
+        double minPrice = 100000;
+        double minPrice2 = 100000;
+        double[] prices = new double[2];
+        for (Table itrTable : getJson2.getTable()) {
+            //if(!itrTable.getClassUser().equals("Авто Радиатор ООО")) {
+
+            price[y] = Double.valueOf(cutPrice(itrTable.getPrice()));
+            if (price[y] < minPrice) {
+                minPrice2 = minPrice;
+                minPrice = price[y];
+            }
+            System.out.println(price[y]);
+
+            // }
+            y++;
+        }
+
+        prices[0] = minPrice;
+        prices[1] = minPrice2;
+        return prices;
+    }
+
+    public static String getCompany(GetJson getJson2) {
+        int y = 0;
+        double[] price = new double[10];
+        double minPrice = 100000;
+        //String company="Авто Радиатор ООО";
+        String company = null;
+
+
+        for (Table itrTable : getJson2.getTable()) {
+
+            price[y] = Double.valueOf(cutPrice(itrTable.getPrice()));
+            if (price[y] < minPrice) {
+                minPrice = price[y];
+                System.out.println(itrTable.getClassMan());
+                System.out.println(itrTable.getInstock());
+                System.out.println(itrTable.getClassUser());
+                company = itrTable.getClassUser();
+            }
+
+
+            y++;
+        }
+
+
+        return company;
+    }
+
+    public static boolean isOnZzap(GetJson getJson2) {
+        String company = "Авто Радиатор ООО";
+
+        for (Table itrTable : getJson2.getTable()) {
+            if (company.equals(itrTable.getClassUser())) {
+                System.out.println(itrTable.getClassUser());
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+
+    private static void wait3() {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //while()
+
+    }
+
+    private static String cutPrice(String s) {
+        s = s.substring(0, s.length() - 2);
+        s = s.replaceAll(" ", "");
+//        for(int i=0;i<s.length();i++){
+//            if(s.charAt(i)==' '){
+//
+//            }
+//        }
+        return s;
     }
 
 
     //обрезка строки
-    private static String cutResponse(String s){
-        while (s.charAt(0)!='{') {
-            s=s.substring(1);
+    private static String cutResponse(String s) {
+        while (s.charAt(0) != '{') {
+            s = s.substring(1);
         }
-        s=s.substring(0,s.length()-9);
+        s = s.substring(0, s.length() - 9);
         return s;
     }
 }
